a2bc82451d7f01d8aaffbede422fc28e91ae2f4d
Dzymin <szymonsobczak5@gmail.com>
2022-10-22 17:06:45 +0200

    Przenosiny backendu do innego repo

--- Backend/Models/Activity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backend.Models
-{
-    public class Activity
-    {
-        public long Id { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-    }
-}
