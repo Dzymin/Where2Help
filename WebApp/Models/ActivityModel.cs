3665981aea90e9821311f623057d9db849b7fd2e
Dzymin <szymonsobczak5@gmail.com>
2022-10-23 23:09:02 +0200

    Poprawki na ostateczny komicior

---
+++ WebApp/Models/ActivityModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp.Models
+{
+    public class ActivityModel
+    {
+        public long? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
